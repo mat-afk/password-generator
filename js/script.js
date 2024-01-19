0f7b47dd8956cc063ab8e603521c025bd364a8e1
mat-afk <mateusc.ramos@outlook.com>
2024-01-19 16:15:47 -0300

    feat: character generation functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log("Hello, world");
+// Selecting elements
+const generatePasswordSpan = document.querySelector("#generate-password");
+const generatedPasswordDiv = document.querySelector("#generated-password");
+
+// Functions
+const getLowercaseLetter = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getUppercaseLetter = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+  const symbols = "[]{}()!@#$%&*_-+=/?><,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+// Event listeners
+generatePasswordSpan.addEventListener("click", () => {
+  console.log(getLowercaseLetter());
+  console.log(getUppercaseLetter());
+  console.log(getNumber());
+  console.log(getSymbol());
+});
